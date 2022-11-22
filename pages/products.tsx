e1e0f6817a8fc201e7243d9646bb574d6c2931a5
yongpilmoon <m71661@gmail.com>
2022-11-22 10:27:38 +0900

    메타 데이터 추가

--- pages/products.tsx
+++ pages/products.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Carousel from 'nuka-carousel/lib/carousel'
 import { useState } from 'react'
+import Head from 'next/head'
 
 const images = [
   {
@@ -37,6 +38,25 @@ function Products() {
   const [index, setIndex] = useState(0)
   return (
     <>
+      <Head>
+        <meta
+          property="og:url"
+          content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html"
+        />
+        <meta property="og:type" content="article" />
+        <meta
+          property="og:title"
+          content="When Great Minds Don’t Think Alike"
+        />
+        <meta
+          property="og:description"
+          content="How much does culture influence creative thinking?"
+        />
+        <meta
+          property="og:image"
+          content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg"
+        />
+      </Head>
       <Carousel
         animation="fade"
         autoplay
