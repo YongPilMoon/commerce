af085992001e0ba0ecc7f1d2a8d09face5b9f2d7
yongpilmoon <m71661@gmail.com>
2022-11-26 10:09:56 +0900

    후기 글 구현

--- pages/products/[id]/index.tsx
+++ pages/products/[id]/index.tsx
@@ -5,7 +5,7 @@ import CustomEditor from '@/components/Editor'
 import { useRouter } from 'next/router'
 import { convertFromRaw, convertToRaw, EditorState } from 'draft-js'
 import { GetServerSideProps, GetServerSidePropsContext } from 'next'
-import { products, Cart, OrderItem } from '@prisma/client'
+import { products, Cart, OrderItem, Comment } from '@prisma/client'
 import { format } from 'date-fns'
 import { CATEGORY_MAP } from 'constants/products'
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
@@ -15,6 +15,7 @@ import { useSession } from 'next-auth/react'
 import CountControl from '@/components/CountControl'
 import { CART_QUERY_KEY } from '../../cart'
 import { ORDER_QUERY_KEY } from 'pages/my'
+import CommentItem from '@/components/CommentItem'
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const product = await fetch(
@@ -22,16 +23,28 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
   )
     .then((res) => res.json())
     .then((data) => data.items)
+
+  const comments = await fetch(
+    `http://localhost:3000/api/get-comments?productId=${context.params?.id}`
+  )
+    .then((res) => res.json())
+    .then((data) => data.items)
   return {
     props: {
       product: { ...product, images: [product.image_url, product.image_url] },
+      comments,
     },
   }
 }
 
 const WISHLIST_QUERY_KEY = '/api/get-wishlist'
 
-function Products(props: { product: products & { images: string[] } }) {
+export interface CommentItemType extends Comment, OrderItem {}
+
+function Products(props: {
+  product: products & { images: string[] }
+  comments: CommentItemType[]
+}) {
   const [index, setIndex] = useState(0)
   const { data: session } = useSession()
   const [quantity, setQuantity] = useState<number | undefined>(1)
@@ -194,6 +207,13 @@ function Products(props: { product: products & { images: string[] } }) {
             {editorState != null && (
               <CustomEditor editorState={editorState} readOnly />
             )}
+            <div>
+              <p className="text-2xl font-semibold">후기</p>
+              {props.comments &&
+                props.comments.map((comment, idx) => (
+                  <CommentItem key={idx} item={comment} />
+                ))}
+            </div>
           </div>
           <div style={{ maxWidth: 600 }} className="flex flex-col space-y-6">
             <div className="text-lg text-zinc-400">
